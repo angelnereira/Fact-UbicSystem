cbe9c3c9bd92194aed5fe4e6651bba9db7c90160
Angel Nereira <angelnereira15@gmail.com>
2025-10-07 11:23:24 -0500

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2913709404-9dd8a",
   "appId": "1:182724375205:web:fb845ca5ffadefa373d34c",
-  "apiKey": "AIzaSyBctx7rS60Ubf6vic6jNWzWiC7j9lG-wyo",
+  "apiKey": "APIKEY_FIREBASE",
   "authDomain": "studio-2913709404-9dd8a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "182724375205"
