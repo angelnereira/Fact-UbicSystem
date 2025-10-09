01b0a241e356c0eb8dadbdedd86a40f2ab936e5d
Ángel Nereira <angelnereira15@gmail.com>
2025-10-09 16:24:49 +0000

    ahora aparece un nuevo error: Runtime FirebaseError
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const firebaseConfig = {
   "projectId": "studio-2913709404-9dd8a",
   "appId": "1:182724375205:web:fb845ca5ffadefa373d34c",
-  "apiKey": process.env.FIREBASE_API_KEY,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-2913709404-9dd8a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "182724375205"
