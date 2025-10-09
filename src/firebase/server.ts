297837e22c8f0b462d288457586571cbc69c08e7
Ángel Nereira <angelnereira15@gmail.com>
2025-10-09 17:11:19 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,5 +1,5 @@
 
-import { initializeApp, getApp, getApps, FirebaseApp, App } from 'firebase-admin/app';
+import { initializeApp, getApp, getApps, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { firebaseConfig } from './config';
