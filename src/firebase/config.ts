23667f8a4d5f1114632002a1cb031ea00681ab4b
Ángel Nereira <angelnereira15@gmail.com>
2025-10-09 16:19:52 +0000

    {   "titulo": "Solución para proteger la clave API de Firebase en src/fi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const firebaseConfig = {
   "projectId": "studio-2913709404-9dd8a",
   "appId": "1:182724375205:web:fb845ca5ffadefa373d34c",
-  "apiKey": "APIKEY_FIREBASE",
+  "apiKey": process.env.FIREBASE_API_KEY,
   "authDomain": "studio-2913709404-9dd8a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "182724375205"
